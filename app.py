b09672aa23f862ce900095bdc228ac8e4dbba119
HuskyHsu <chessman416@hotmail.com>
2018-06-11 21:15:27 +0800

    use sqlite

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, Response
 import requests
+import sqlite3
 
 app = Flask(__name__)
 
@@ -9,23 +10,12 @@ def get_bbox_sites(lat, lng):
     lat = float(lat)
     lng = float(lng)
 
-    headers = {
-        "X-Requested-With":"XMLHttpRequest",
-        "Referer":"https://www.pokemongomap.info",
-        "User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X) App leWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D257 Safari/9537.53",
-        "Cookie": '__cfduid=dc1fdf477a64225cd6b8005548b5176ec1527300954; _ga=GA1.2.527083445.1527300954; disqus_unique=6olkqof3rt88l0; PHPSESSID=4p8kdvr2gdnq6c1vh6gd0ldde0; announcementnews17=1; mapfilters=1[##split##]1[##split##]1[##split##]0[##split##]0[##split##]1[##split##]0[##split##]0[##split##]1[##split##]1[##split##]1[##split##]0[##split##]1[##split##]1[##split##]1[##split##]1[##split##]1[##split##]0[##split##]1[##split##]6[##split##]0[##split##]2[##split##]1[##split##]0[##split##]0[##split##]0[##split##]0[##split##]1[##split##]; _gid=GA1.2.1164560502.1528201848; alertmsg4=1; __atuvc=15%7C21%2C5%7C22%2C11%7C23; __atuvs=5b16827738947956001; __atssc=facebook%3B1; __jid=7jgtgk93sqie3j; latlngzoom=17[##split##]25.059815932803865[##split##]121.52229168913263'
-    }
+    conn = sqlite3.connect('pokestop.db')
+    c = conn.cursor()
 
-    payload = {
-        "fromlat": lat - 0.001,
-        "fromlng": lng - 0.001,
-        "tolat": lat + 0.001,
-        "tolng": lng + 0.001
-    }
-    
-    r = requests.post("https://www.pokemongomap.info/includes/it77nmsq9.php", data=payload, headers=headers)
+    data = [row for row in c.execute("select * from pokestop where lat between ? and ? and lng between ? and ?", (lat - 0.001, lat + 0.001, lng - 0.001, lng + 0.001))]
 
-    return jsonify(r.text)
+    return jsonify(data)
 
 @app.route("/get_PokeStops/<name>/<lat>/<lng>")
 def get_PokeStops(name, lat, lng):
